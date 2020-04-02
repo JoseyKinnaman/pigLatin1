3fb679787a2269698eaf4cf145938ba0a4f968b9
Jozy Kinnaman <jozypants@gmail.com>
2020-04-01 17:14:09 -0700

    update README with more details regarding project.

--- js/scripts.js
+++ js/scripts.js
@@ -7,38 +7,35 @@ function pigLatin(userInput) {
   for (i = 0; i < words.length; i++) {
     var word = words[i].split("");
     // console.log(word);
-
-    if (word[0] === "a" || word[0] === "i" || word[0] === "o" || word[0] === "e" || word[0] === "u") {
-      newArr.push(word.join("") + "way ") 
-      console.log(newArr);
-    } else {
-      // if this is a consonant:
-      // remove the first letter and store it in a var
-      // put the first letter at the end
-      // add ay
-
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-        // console.log (word)
-        if (word[0] === "q" && word[1] === "u") {
-          var qU = word.splice(0,2);
-          newArr.push(word.concat(qU).join("") + "ay");
-          // word.push(qU[1]);
-          console.log(newArr);
-        }
-
-        
-      
-    //   //remove "qu" from beginning of word and hold in a variable
-    //   // push the removed letters onto the end
-    //   //.join() convert array into a string
-    // }
-      }
   }
-}
+  if (word[0] === "a" || word[0] === "i" || word[0] === "o" || word[0] === "e" || word[0] === "u") {
+    newArr.push(word.join("") + "way ")
+    console.log(newArr);
+  } else {
+    // if this is a consonant:
+    // remove the first letter and store it in a var
+    // put the first letter at the end
+    // add ay
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    // console.log (word)
+  
+      if (word[0] === "q" && word[1] === "u") {
+        var qU = word.splice(0, 2);
+        newArr.push(word.concat(qU).join("") + "ay");
+        // word.push(qU[1]);
+      } else {
+        var consonants = word.shift();
+        console.log(consonants);
+        word.push(consonants.join("") + "ay");
+        console.log(word);
+      }
+          
+      }
+      //   //.join() convert array into a string
+    
 
 
-// user interface logic aka "the party"
-$(document).ready(function () {
   $("form#pigForm").submit(function (event) {
     event.preventDefault();
     var userInput = $("input#userInput").val();
